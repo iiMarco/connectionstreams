d609f58d27347ef8475ba23567f9ff1bc90647ae
Mark Zammit <iimarco@me.com>
2013-10-21 15:22:57 +1100

    Update connstream.h

--- connstream.h
+++ connstream.h
@@ -1,6 +1,7 @@
 /*
  * Author   : Mark Zammit
- * Contact  : markzammit@me.com
+ * Contact  : iimarco@me.com
+ * Version  : 1.13.11.21
  */
 
  /** Connection Stream Interface
@@ -37,7 +38,7 @@
 #if defined(UNICODE) || defined(_UNICODE_)
 
 #ifndef TSTR
-#define TSTR	std::wstring
+#define TSTR    std::wstring
 #endif
 
 #ifndef _T(x)
@@ -48,7 +49,7 @@
 #else
 
 #ifndef TSTR
-#define	TSTR    std::string
+#define    TSTR    std::string
 #endif
 
 #ifndef _T(x)
@@ -61,6 +62,9 @@
 #define INVALID_FILE    -1
 
 #include <string>
+#include <vector>
+
+#define LIST    std::vector<TSTR>
 
 class connstream
 {
@@ -69,103 +73,125 @@ class connstream
         virtual ~connstream(void) {};
     public:
         /// CONNECTION METHODS ///
-		/** bool connect(TSTR, TSTR, TSTR)
-		 *  Connects/Opens to stream.
-		 *      @url    : Fully qualified connection path mandatory
-		 *      @uid    : User id optional
-		 *      @pwd    : Password optional
-		 *      @port   : Port optional
-		 *  Returns : @true on connect of @false on failure
-		 */
-		 /** bool disconect(void)
-		 *  Disconnects/Closes open stream.
-		 *
-		 *  Returns : @true on successful disconnect or @false if already disconnected
-		 */
-		virtual bool connect(TSTR, TSTR = _T(""), TSTR = _T(""), int = 0) = 0;
-		virtual bool disconnect(void) = 0;
+        /** bool Connect(TSTR, TSTR, TSTR)
+         *  Connects/Opens to stream.
+         *      @url    : Fully qualified connection path mandatory
+         *      @uid    : User id optional
+         *      @pwd    : Password optional
+         *      @port   : Port optional
+         *  Returns : @true on connect of @false on failure
+         */
+        /** bool disconect(void)
+         *  Disconnects/Closes open stream.
+         *
+         *  Returns : @true on successful disconnect or @false if already disconnected
+         */
+        virtual bool Connect(TSTR, TSTR = _T(""), TSTR = _T(""), int = 0) = 0;
+        virtual bool Disconnect(void) = 0;
 
-		/// GET/PUSH METHODS ///
-		/** Uploads a file from host location to host location.
-		 *      @location   : Client relative file location
-		 *      @rename     : File name to be saved on host
-		 *  Returns : @true on successful upload or @false on failed upload, could be
-		 *              because file already exists on host or because there is no active connection.
-		 */
-		 /** Downloads a file from host location to client location.
-		 *      @location   : Client relative file location
-		 *      @rename     : File name to be saved on host
-		 *  Returns : @true on successful upload or @false on failed upload, could be
-		 *              because file already exists on client or because there is no active connection.
-		 */
-		virtual bool upload(TSTR, TSTR) = 0;
-		virtual bool download(TSTR, TSTR) = 0;
+        /// GET/PUSH METHODS ///
+        /** Uploads a file from host location to host location.
+         *      @location   : Client relative file location
+         *      @rename     : File name to be saved on host
+         *  Returns : @true on successful upload or @false on failed upload, could be
+         *              because file already exists on host or because there is no active connection.
+         */
+        /** Downloads a file from host location to client location.
+         *      @location   : Client relative file location
+         *      @rename     : File name to be saved on host
+         *  Returns : @true on successful upload or @false on failed upload, could be
+         *              because file already exists on client or because there is no active connection.
+         */
+        virtual bool Upload(TSTR, TSTR) = 0;
+        virtual bool Download(TSTR, TSTR) = 0;
 
-		/// DIRECTORY METHODS ///
-		/** bool cd(TSTR)
-		 *  Change Directory to either a fully qualified path or relative path.
-		 *      @dir  : Directory on host to change to
-		 *  Returns : @true on succcessful change or @false if no directory exists or
-		 *              if there is no active connection.
-		 */
-		 /** bool mkdir(TSTR)
-		 *  Creates Directory for either a fully qualified path or relative path.
-		 *      @dir  : Directory on host to create
-		 *  Returns : @true on succcessful directory creation or @false if create failed or
-		 *              if there is no active connection.
-		 */
-		 /** bool rmdir(TSTR)
-		 *  Removes Directory for either a fully qualified path or relative path.
-		 *      @dir  : Directory on host to remove
-		 *  Returns : @true on succcessful removal or @false if directory doesn't exist or
-		 *              if there is no active connection.
-		 */
-		 /** TSTR getcd(void)
-		 *  Retrieves current directory path
-		 *
-		 *  Returns : currently pointed to host directory as a TSTR for unicode support
-		 */
-		virtual bool cd(TSTR) = 0;
-		virtual bool mkdir(TSTR) = 0;
-		virtual bool rmdir(TSTR) = 0;
-		virtual TSTR getcd(void) = 0;
+        /// DIRECTORY METHODS ///
+        /** bool ChangeDirectory(TSTR)
+         *  Change Directory to either a fully qualified path or relative path.
+         *      @dir  : Directory on host to change to
+         *  Returns : @true on succcessful change or @false if no directory exists or
+         *              if there is no active connection.
+         */
+        /** bool MakeDir(TSTR)
+         *  Creates Directory for either a fully qualified path or relative path.
+         *      @dir  : Directory on host to create
+         *  Returns : @true on succcessful directory creation or @false if create failed or
+         *              if there is no active connection.
+         */
+        /** bool RemoveDir(TSTR)
+         *  Removes Directory for either a fully qualified path or relative path.
+         *      @dir  : Directory on host to remove
+         *  Returns : @true on succcessful removal or @false if directory doesn't exist or
+         *              if there is no active connection.
+         */
+        /** TSTR CurrentDir(void)
+         *  Retrieves current directory path
+         *
+         *  Returns : currently pointed to host directory as a TSTR for unicode support
+         */
+        /** std::vector<TSTR> SearchDir(TSTR)
+         *  Gets a directory listing based off a system valid search string.
+         *      @searchstr  : Search string thats system valid, e.g. *.txt, file.txt, *.* etc.
+         *  Returns a vector list of file names.
+         */
+        virtual bool    ChangeDir(TSTR) = 0;
+        virtual bool    MakeDir(TSTR) = 0;
+        virtual bool    RemoveDir(TSTR) = 0;
+        virtual TSTR    CurrentDir(void) = 0;
+        virtual LIST    SearchDir(TSTR) = 0;
 
         /// FILE HANDLING METHODS ///
-        /** bool remove(TSTR)
+        /** bool Remove(TSTR)
          *  Removes a file from FTP host.
          *      @filename   : Name of file to be deleted on host.
          *  Returns : @true of file was successfully removed or @false if no file exists or
          *              if there is no active connection.
          */
-         /** bool rename(TSTR, TSTR)
+        /** bool Rename(TSTR, TSTR)
          *  Renames a file on FTP host.
          *      @oldFilename    : Name of file to be renamed on host.
          *      @newFilename    : Name for file to be renamed to on host.
          *  Returns : @true of file was successfully renamed or @false if no file exists or
          *              if there is no active connection.
          */
-         /** LONGLONG getfilesize(TSTR)
+        /** bool Exists(TSTR)
+         *  Checks whether a file exists on the host.
+         *      @filename   : Name of file to check existence of.
+         *  Returns : @true if file is present or @false if no file exists or
+         *              if there is no active connection.
+         */
+        /** LONGLONG GetFileSize(TSTR)
          *  File size of existing host file.
          *      @filename   : Name of file to retrieve size of on host.
          *  Returns : File size as a long long or @INVALID_FILE if file does not exist or
          *              if there is no active connection.
          */
-		virtual bool        remove(TSTR) = 0;
-		virtual bool        rename(TSTR, TSTR) = 0;
-		virtual long long   getfilesize(TSTR) = 0;
+        virtual bool        Remove(TSTR) = 0;
+        virtual bool        Rename(TSTR, TSTR) = 0;
+        virtual bool        Exists(TSTR) = 0;
+        virtual long long   GetFileSize(TSTR) = 0;
 
         /// MISCELLANEOUS METHODS ///
-         /** bool command(TSTR)
+        /** bool Command(TSTR)
          *  Executes a user defined command based on stream type
          *      @command    : Command to be executed based of stream interface.
          *
          *  Returns : @true if command was successfully exeucted or @false if not or
          *              if there is no active connection.
          */
-		virtual bool command(TSTR) = 0;
+        virtual bool Command(TSTR) = 0;
+
+        /** int GetLastError()
+         *  Retrieves the last error (return) value executed by the last method call.
+         *  If a method returned false then this method can be called for more detail.
+         *
+         *  Returns : 0x00 on successful last method call, or 0x01+ for a failed/info result.
+         */
+        virtual int GetLastError(void) = 0;
 
     protected:
         bool    m_connected;
+        int     m_err;
 };
 
 #endif // _CONNSTREAM_H
